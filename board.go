187329a140767fa1b6ae7712c15c751587278b10
rosswf <ross@rossw.co.uk>
2022-08-05 18:15:02 +0100

    Implement Stringer for board

--- board.go
+++ board.go
@@ -2,12 +2,18 @@ package tictactoe
 
 import (
 	"errors"
+	"fmt"
+	"strings"
 )
 
 type Board [3][3]rune
 
-func NewBoard() *Board {
-	return &Board{}
+func NewBoard() Board {
+	return Board{
+		{' ', ' ', ' '},
+		{' ', ' ', ' '},
+		{' ', ' ', ' '},
+	}
 }
 
 func (b *Board) playMove(x, y int, player rune) error {
@@ -15,10 +21,21 @@ func (b *Board) playMove(x, y int, player rune) error {
 		return errors.New("Invalid location")
 	}
 
-	if b[x][y] != 0 {
+	if b[x][y] != ' ' {
 		return errors.New("Invalid move")
 	}
 
 	b[x][y] = player
 	return nil
+}
+
+func (b Board) String() string {
+	var sb strings.Builder
+	for i, row := range b {
+		if i != 0 {
+			fmt.Fprint(&sb, "--+---+--\n")
+		}
+		fmt.Fprintf(&sb, "%c | %c | %c\n", row[0], row[1], row[2])
+	}
+	return sb.String()
 }
