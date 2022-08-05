883738b1cd213e2c4968ca90136846e1615ee14b
rosswf <ross@rossw.co.uk>
2022-08-05 17:38:50 +0100

    Check for valid board locations

--- board.go
+++ board.go
@@ -1,6 +1,8 @@
 package tictactoe
 
-import "errors"
+import (
+	"errors"
+)
 
 type Board [3][3]rune
 
@@ -8,10 +10,15 @@ func NewBoard() *Board {
 	return &Board{}
 }
 
-func (b *Board) PlayMove(x, y int, player rune) error {
+func (b *Board) playMove(x, y int, player rune) error {
+	if !(x < 3 && y < 3) {
+		return errors.New("Invalid location")
+	}
+
 	if b[x][y] != 0 {
 		return errors.New("Invalid move")
 	}
+
 	b[x][y] = player
 	return nil
 }
